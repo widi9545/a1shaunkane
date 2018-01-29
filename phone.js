3af327e278408a33d486067f5d3c582c93bdaaf7
widi9545 <widi9545@colorado.edu>
2018-01-28 19:41:26 -0700

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,12 @@
+
+
+
+$(document).ready(function() { // do this when the document is loaded
+	$("#element").show(); // show the element with ID "element"
+	$("#otherElement").hide(); // hide the element with ID "otherElement"
+});
+
+$("#button_id").click(function() { // when "button_id" is clicked
+	$("#element").show(); // show element
+	$("#other_element").hide();	// hide other element
+});
